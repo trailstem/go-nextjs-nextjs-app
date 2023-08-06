1c43bef30d0192714499d8b100d8d6fc7b2a68d9
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-08-06 23:59:03 +0900

    todo画面の作成

---
+++ app/todo/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import Todo from '../components/Todo'
+
+export default function TodoPage() {
+  return (
+    <><div className='flex justify-center'><p>todoです</p></div><Todo /></>
+
+  )
+}
